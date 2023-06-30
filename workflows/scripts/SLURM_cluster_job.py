07a9a874d1cf314560eb0b89c2592ee58733e997
Santiago Castro Dau <sanntiago5@gmail.com>
2023-06-30 18:46:26 +0200

    lowerd time to see if i can get the job to que in a shorter queue

--- workflows/scripts/SLURM_cluster_job.py
+++ workflows/scripts/SLURM_cluster_job.py
@@ -17,7 +17,7 @@ rule_name = job_properties["rule"]
 
 # Get time
 if queue == "x86_1h":
-    time = "60:00"
+    time = "59:99"
 elif queue == "x86_6h":
     time = "6:00:00"
 elif queue == "x86_24h":
